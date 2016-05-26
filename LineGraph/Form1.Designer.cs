888e6efd5e24c6363e1fb2ca6335f25adb5f45b3
totalintelli <blueintelli@gmail.com>
2016-05-26 17:10:11 +0900

    제목과 (만 권)을 그림.

--- LineGraph/Form1.Designer.cs
+++ LineGraph/Form1.Designer.cs
@@ -33,11 +33,13 @@
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.PaleTurquoise;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(841, 578);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Form1
             // 
--- LineGraph/Form1.cs
+++ LineGraph/Form1.cs
@@ -17,5 +17,29 @@ namespace LineGraph
             InitializeComponent();
         }
 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // 제목 글자열
+            string Title = "월별 책의 판매량";
+            // 제목의 폰트
+            Font TitleFont = new Font("SansSerif", 18);
+            // 제목의 색상
+            SolidBrush TitleBrush = new SolidBrush(Color.Blue);
+            // 제목의 위치
+            PointF TitlePoint = new PointF(panel1.Width / 2.0f, 0); // 1/ 2.0f는 제목의 위치를 정하기 위한 값으로 고정값.
+            // Y축 글자열
+            // (만 권)
+            string KwanText = "(만 권)";
+            // 글자의 색상
+            SolidBrush TextBrush = new SolidBrush(Color.Teal);
+            // 글자의 위치
+            PointF KwanTextPoint = new PointF(panel1.Width * 0.1f, panel1.Height * 0.1f);
+
+            // 제목을 그린다.
+            e.Graphics.DrawString(Title, TitleFont, TitleBrush, TitlePoint);
+
+            // (만 권)을 그린다.
+            e.Graphics.DrawString(KwanText, TitleFont, TextBrush, KwanTextPoint);
+        }
     }
 }
